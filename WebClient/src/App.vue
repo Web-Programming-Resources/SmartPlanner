490c2cc153926f703a0fabb0f038b7a2f8c3dbba
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2018-10-17 18:46:52 +0200

    Added simple navbar and footer

--- WebClient/src/App.vue
+++ WebClient/src/App.vue
@@ -1,60 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <b-navbar sticky toggleable="md" type="dark" variant="info">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand>Smart Planner</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown v-bind:text="lang" right>
+          <b-dropdown-item v-on:click="lang='pl'">pl</b-dropdown-item>
+          <b-dropdown-item v-on:click="lang='en'">en</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+    <b-navbar fixed="bottom" toggleable="md" type="dark" variant="info">
+        <b-navbar-brand class="footer">Copyright&copy; 2018 by GKN. All rights reserved.</b-navbar-brand>
+    </b-navbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      lang: String
     }
+  },
+  created() {
+    this.lang = "pl";
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped>
+  .footer {
+    font-style: italic;
+    opacity: 0.25;
+  }
 </style>
