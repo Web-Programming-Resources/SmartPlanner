b259ca504347a3f787cd8c469b2d3c0e0d30827d
Jakub Gros <kubagros@gmail.com>
2018-11-03 12:52:33 +0100

    init commit

---
+++ Back-end/src/SmartPlanner.java
@@ -0,0 +1,7 @@
+public class SmartPlanner {
+
+    //komiwojażer
+    //każdy wierzcholek to jedna instancja danej aktywnosci
+    //kazda aktywnosc przechowujemy w tablicy wraz z jej czasem trwania i czasem transportu do kazdej innej aktywnosci
+
+}
