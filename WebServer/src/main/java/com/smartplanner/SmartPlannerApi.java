e4e6ffd6a698073c79a41573e51c73d881024d72
nowakkamil <nowakkamil@yahoo.com>
2019-01-04 21:15:02 +0100

    Change the method's name and perform minor formatting

--- WebServer/src/main/java/com/smartplanner/SmartPlannerApi.java
+++ WebServer/src/main/java/com/smartplanner/SmartPlannerApi.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.smartplanner.repository")
 public class SmartPlannerApi {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartPlannerApi.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPlannerApi.class, args);
+    }
 }
