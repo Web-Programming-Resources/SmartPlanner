740f2edf2994a29c93a9c26caccb9c8d34353b97
Jakub Gros <kubagros@gmail.com>
2019-01-04 19:03:55 +0100

    made timetable validation computations multithreaded

--- Back-end/src/com/smartplanner/SmartPlanner.java
+++ Back-end/src/com/smartplanner/SmartPlanner.java
@@ -1,8 +1,7 @@
 package com.smartplanner;
 
 import java.util.ArrayList;
-
-
+import java.util.List;
 
 
 public class SmartPlanner {
@@ -34,11 +33,46 @@ public class SmartPlanner {
 
         TimetableValidator validator = new TimetableValidator(distManag);
 
-        ArrayList<ArrayList<TimetableEntry>> validTimetables = new ArrayList<ArrayList<TimetableEntry>>();
 
-        for(ArrayList<TimetableEntry> timetable : potentialTimetables)
-            if(validator.isValid(timetable))
-                validTimetables.add(timetable);
+        int halfIndex = potentialTimetables.size()/2;
+
+        List<ArrayList<TimetableEntry>> firstHalfOfPotentialTimetables = potentialTimetables.subList(0,halfIndex);
+        List<ArrayList<TimetableEntry>>  secondHalfOfPotentialTimetables = potentialTimetables.subList(halfIndex, potentialTimetables.size());
+        ArrayList<ArrayList<TimetableEntry>> firstHalfOfValidTimetables = new ArrayList<ArrayList<TimetableEntry>>();
+        ArrayList<ArrayList<TimetableEntry>> secondHalfOfValidTimetables = new ArrayList<ArrayList<TimetableEntry>>();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(ArrayList<TimetableEntry> timetable : firstHalfOfPotentialTimetables)
+                    if(validator.isValid(timetable))
+                        firstHalfOfValidTimetables.add(timetable);
+            }
+        });
+
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(ArrayList<TimetableEntry> timetable : secondHalfOfPotentialTimetables)
+                    if(validator.isValid(timetable))
+                        secondHalfOfValidTimetables.add(timetable);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        ArrayList<ArrayList<TimetableEntry>> validTimetables = new ArrayList<ArrayList<TimetableEntry>>(firstHalfOfValidTimetables);
+        validTimetables.addAll(secondHalfOfValidTimetables);
 
         TimetableWithDecisionPointsAndScore bestTimetable = new TimetableWithDecisionPointsAndScore(0, null, null);
         if(validTimetables.size() == 0)
@@ -46,6 +80,7 @@ public class SmartPlanner {
 
         OptimalityCalculator optimalityCalculator = new OptimalityCalculator(distManag, maxCommutesPerDay,
                 minTimeSpentAtOptimizedAtOnceInMinutes, daysInCycle, optimizedActivity);
+
 
         for(ArrayList<TimetableEntry> validTimetable : validTimetables) {
             TimetableWithDecisionPointsAndScore currentTimetable = optimalityCalculator.calculate(validTimetable);
