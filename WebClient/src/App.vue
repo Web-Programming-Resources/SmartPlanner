a9f071abc24db0c30265b20224d626b7de280bda
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2019-01-11 15:44:39 +0100

    Fixed routes

--- WebClient/src/App.vue
+++ WebClient/src/App.vue
@@ -29,6 +29,7 @@ export default {
         headers:
         {
           logout: "",
+          displayPlans: "",
           createPlan: "",
         }
       },
--- WebClient/src/routes.js
+++ WebClient/src/routes.js
@@ -1,5 +1,10 @@
 import Login from './subpages/Login/'
+<<<<<<< HEAD
 import MyPlans from './subpages/MyPlans/'
+=======
+import DisplayPlans from './subpages/DisplayPlans/'
+import CreatePlan from './subpages/CreatePlan/'
+>>>>>>> Fixed routes
 
 export const routes = [
     {
@@ -8,6 +13,10 @@ export const routes = [
     },
     {
         path: '/',
+<<<<<<< HEAD
         component: MyPlans
+=======
+        component: DisplayPlans
+>>>>>>> Fixed routes
     }
 ]
