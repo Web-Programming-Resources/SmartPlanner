9d4659c2fbd73d0ba4b098bac606348eade5da22
nowakkamil <nowakkamil@yahoo.com>
2019-01-13 01:22:56 +0100

    Add Swagger UI Documentation

--- WebServer/src/main/java/com/smartplanner/config/WebSecurityConfig.java
+++ WebServer/src/main/java/com/smartplanner/config/WebSecurityConfig.java
@@ -48,7 +48,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/token/generate", "/api/users/signup").permitAll()
+                .antMatchers(
+                        "/api/token/generate",
+                        "/api/users/signup",
+                        "/v2/api-docs",
+                        "/swagger-ui**",
+                        "/swagger-resources/**",
+                        "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
