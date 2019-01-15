4ff8f20e8cdbf96147f49810d5fc58298bc42172
Jakub Gros <kubagros@gmail.com>
2019-01-15 03:16:06 +0100

    fixed package name

--- MobileApp/app/src/androidTest/java/com/smartplanner/ExampleInstrumentedTest.java
+++ MobileApp/app/src/androidTest/java/com/smartplanner/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ledwon.jakub.smartplanner", appContext.getPackageName());
+        assertEquals("com.smartplanner", appContext.getPackageName());
     }
 }
